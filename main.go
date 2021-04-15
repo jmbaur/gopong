7305b9cf53b8e498a1c6e14dda09163e96d3d718
Jared Baur <jaredbaur@fastmail.com>
2021-04-14 20:03:45 -0700

    Get angle reflections working

--- main.go
+++ main.go
@@ -25,7 +25,6 @@ type entity struct {
 }
 
 func (e *entity) update() error {
-	fmt.Println(e.x, e.y)
 	e.element.Get("style").Call("setProperty", "left", e.x-(e.width/2))
 	e.element.Get("style").Call("setProperty", "top", e.y-(e.height/2))
 	return nil
@@ -44,20 +43,20 @@ func main() {
 	// Get a random initial angle for the ball where it will be moving away
 	// from the paddle within a 90 degree window.
 	ballTheta0 = ((rand.Float64() * 90) - 45) * math.Pi / 180
+	// ballTheta0 = (180) * math.Pi / 180
 
 	document := js.Global().Get("document")
 	window := js.Global().Get("window")
 
-	width := window.Get("innerWidth").Float()
-	height := window.Get("innerHeight").Float()
-	fmt.Println("width", width, "height", height)
+    width := window.Get("innerWidth").Float()
+    height := window.Get("innerHeight").Float()
 
 	ball := &entity{
 		x:       (width) / 2,
 		y:       (height) / 2,
 		width:   ballDiameter,
 		height:  ballDiameter,
-		speed:   10.0,
+		speed:   12.0,
 		angle:   ballTheta0,
 		element: document.Call("getElementById", "ball"),
 	}
@@ -68,36 +67,35 @@ func main() {
 	ball.element.Get("style").Call("setProperty", "border-radius", "50%")
 	ball.update()
 
-	paddle := &entity{
-		x:       0,
-		y:       0,
-		speed:   2.0,
-		angle:   0.0,
-		element: document.Call("getElementById", "paddle"),
-	}
-	paddle.element.Get("style").Call("setProperty", "position", "absolute")
-	paddle.element.Get("style").Call("setProperty", "background-color", "#ff0000")
-	paddle.element.Get("style").Call("setProperty", "width", "2em")
-	paddle.element.Get("style").Call("setProperty", "height", "10em")
+	// paddle := &entity{
+	// 	x:       0,
+	// 	y:       0,
+	// 	speed:   2.0,
+	// 	angle:   0.0,
+	// 	element: document.Call("getElementById", "paddle"),
+	// }
+	// paddle.element.Get("style").Call("setProperty", "position", "absolute")
+	// paddle.element.Get("style").Call("setProperty", "background-color", "#ff0000")
+	// paddle.element.Get("style").Call("setProperty", "width", "2em")
+	// paddle.element.Get("style").Call("setProperty", "height", "10em")
 
 	for {
 		x, y := ball.getNextPosition()
-		// TODO: check that ball hasn't collided
+        // collision with left wall
 		if x-ballRadius <= 0 {
-			fmt.Println("collide with left side of wall")
-			os.Exit(0)
+			fmt.Println("YOU LOSE")
+            os.Exit(0)
 		}
 		if x+ballRadius >= width {
-			fmt.Println("collide with right side of wall")
-			os.Exit(0)
-		}
-		if y-ballRadius <= 0 {
-			fmt.Println("collide with top side of wall")
-			os.Exit(0)
-		}
-		if y+ballRadius >= height {
-			fmt.Println("collide with bottom side of wall")
-			os.Exit(0)
+            // collision with right wall
+            if ball.angle > 0 {
+                ball.angle  = ball.angle + math.Pi - (2 * ball.angle)
+            } else {
+                ball.angle  = ball.angle - math.Pi - (2 * ball.angle)
+            }
+		} else if y-ballRadius <= 0 || y+ballRadius >= height {
+            // collision with top or bottom wall
+            ball.angle  = ball.angle * -1
 		}
 		ball.x = x
 		ball.y = y
